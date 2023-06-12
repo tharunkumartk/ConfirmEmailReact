0c3fc5e69b41f21c93aa2290baa9d6b7d6d9fbcb
TharunTK <tharuntk@princeton.edu>
2023-06-12 12:00:52 +0300

    removed errors

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import logo from "./logo.svg";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import "./App.css";
 import HomePage from "./components/HomePage";
-import { useSearchParams } from "react-router-dom";
-import { useEffect } from "react";
 
 function App() {
   return <HomePage />;
