a9344f8c9a6845c65ebcd6f6abbce62c1cd5572c
TharunTK <tharuntk@princeton.edu>
2023-06-13 20:09:06 +0300

    update app

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -56,7 +56,7 @@ const HomePage = () => {
               },
             }}
           >
-            Open app and confirm email
+            Open app
           </Button>
         </Grid>
       </Grid>
