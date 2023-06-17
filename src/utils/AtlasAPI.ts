d3fede0236fd63dd796afef2d36ff38534487afb
TharunTK <tharuntk@princeton.edu>
2023-06-17 18:06:17 +0300

    update

--- src/utils/AtlasAPI.ts
+++ src/utils/AtlasAPI.ts
@@ -5,9 +5,8 @@ export const confirmUser = async (token: string, tokenId: string) => {
     const app = Realm.App.getApp("data-jrnnm");
     console.log("confirming user...");
     await app.emailPasswordAuth.confirmUser({
-      token:
-        "660df619b93605e1cfb3cbb47fab49413bc9085c556e72002ca768a0c6e402cd3e8ce10d61e81da033ba1c035d2f2bd74e1152cbbc29f19176d9da83144f8df4",
-      tokenId: "648dc46ae63f0f06bce83cfd",
+      token,
+      tokenId,
     });
     console.log("confirmed user!");
     return true;
